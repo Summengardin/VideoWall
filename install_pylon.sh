53a45cbf052f06b83965983bb26cf025fd94fb16
Martin Simengård <summengardin@gmail.com>
2024-04-17 11:57:50 +0200

    Update install_pylon.sh

--- install_pylon.sh
+++ install_pylon.sh
@@ -3,7 +3,7 @@
 # Install pylon SDK
 echo Installing Pylon SDK
 if [ $(dpkg-query -W -f='${Status}' pylon | grep -c 'ok installed') -eq 0 ]; then
-    #wget https://www2.baslerweb.com/media/downloads/software/pylon_software/pylon-7.4.0.14900_linux-x86_64_setup.tar.gz && \
+    wget https://www2.baslerweb.com/media/downloads/software/pylon_software/pylon-7.4.0.14900_linux-x86_64_setup.tar.gz && \
     mkdir ./pylon_setup && \
     tar -C ./pylon_setup -xzf ./pylon-*_setup.tar.gz
     cd ./pylon_setup && \
