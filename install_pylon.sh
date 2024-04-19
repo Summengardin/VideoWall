0f7f09ad8cc98b0b06d7680f4218e8d600e05bf4
Martin Simengård <summengardin@gmail.com>
2024-04-19 07:58:42 +0200

    Update install pylon

--- install_pylon.sh
+++ install_pylon.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-# Install pylon SDK
-echo Installing Pylon SDK
-if [ $(dpkg-query -W -f='${Status}' pylon | grep -c 'ok installed') -eq 0 ]; then
-    wget https://www2.baslerweb.com/media/downloads/software/pylon_software/pylon-7.4.0.14900_linux-x86_64_setup.tar.gz && \
-    mkdir ./pylon_setup && \
-    tar -C ./pylon_setup -xzf ./pylon-*_setup.tar.gz
-    cd ./pylon_setup && \
-    sudo mkdir -p /opt/pylon && \
-    sudo tar -C /opt/pylon -xzf ./pylon-*.tar.gz
-    sudo chmod 755 /opt/pylon
+# Check architecture
+ARCH=$(dpkg --print-architecture)
 
+if [[ $ARCH == "amd64" ]]; then
+    URL="https://www2.baslerweb.com/media/downloads/software/pylon_software/pylon-7.4.0.14900_linux_x86_64_debs.tar.gz"
+elif [[ $ARCH == "arm64" ]]; then
+    URL="https://www2.baslerweb.com/media/downloads/software/pylon_software/pylon-7.4.0.14900_linux_aarch64_debs.tar.gz"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+# Install pylon SDK
+if [ $(dpkg-query -W -f='${Status}' pylon | grep -c 'ok installed') -eq 0 ]; then
+    echo Installing Pylon SDK
+    mkdir ./pylon_setup && cd pylon_setup && \
+    
+    wget $URL && \
+    tar -xzf pylon_*_debs.tar.gz
+    
+    sudo apt install ./pylon_*.deb -y
+ 
     if [ $(dpkg-query -W -f='${Status}' pylon | grep -c 'ok installed') -eq 0 ]; then
-        echo Pylon installed
-    else
         echo Failed to install Pylon
+    else
+        echo Pylon installed
+        cd .. && sudo rm -r ./pylon_setup
     fi
 else
     echo Pylon is already installed
