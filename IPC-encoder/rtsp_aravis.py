e5886faa509238d417c7066439459266dde780cf
Martin Simengård <martin.simengard@seaonics.com>
2024-06-03 13:35:06 +0200

    backup

--- IPC-encoder/rtsp_aravis.py
+++ IPC-encoder/rtsp_aravis.py
@@ -1,46 +1,65 @@
-#!/bin/usr/python3
+#!/usr/bin/env python3
 
 import gi
+import multiprocessing
 gi.require_version('Gst', '1.0')
 gi.require_version('GstRtspServer', '1.0')
 from gi.repository import Gst, GLib, GstRtspServer
 
 class VideoServerRTSP:
 
-    def __init__(self):
+    def __init__(self, port='8554'):
+        self.port = port
 
-        self.loop = self._generate_gst_rtsp_server()
+    def start(self):
+        self.loop = self._generate_gst_rtsp_server(self.port)
         self.loop.run()
 
-    def _generate_gst_rtsp_server(self):
-
+    def _generate_gst_rtsp_server(self, port):
         Gst.init(None)
 
-        pipeline = (
-            "aravissrc ! video/x-raw,format=RGB,width=1280,height=720,framerate=30/1 ! "
+        if port == '8554':
+            pipeline = (
+                "aravissrc ! video/x-bayer,width=1280,height=720,framerate=30/1,format=rggb ! "
+                "queue max-size-time=0 max-size-bytes=0 max-size-buffers=1 ! tcamconvert ! "
+                "videoconvert ! video/x-raw,format=NV12 ! vaapih264enc max-qp=30 bitrate=5000 cpb-length=1 quality-level=1 ! "
+                "queue max-size-time=0 max-size-bytes=0 max-size-buffers=1 ! rtph264pay name=pay0 pt=96"
+            )
+        else:
+            pipeline = (
+            "videotestsrc ! video/x-raw,format=RGB,width=1280,height=720,framerate=30/1 ! "
             "videoconvert ! video/x-raw,format=NV12 ! vaapih264enc ! rtph264pay name=pay0 pt=96"
         )
 
-        #pipeline = "v4l2src device=/dev/video0 ! videoconvert ! videoscale ! video/x-raw,width=[1,640],height=[1,480] ! x264enc tune=zerolatency bitrate=250 ! rtph264pay pt=96 name=pay0"
-        # gst-launch-1.0 aravissrc ! video/x-raw,format=RGB,width=1280,height=720,framerate=30/1 ! videoconvert ! video/x-raw,format=I420 ! vaapih264enc ! fakesink
-        # gst-launch-1.0 videotestsrc ! video/x-raw,format=I420 ! vaapih264enc ! fakesink
-
         server = GstRtspServer.RTSPServer.new()
-        server.set_service(str('5050'))
+        server.set_service(str(port))
         mounts = server.get_mount_points()
 
         factory = GstRtspServer.RTSPMediaFactory.new()
         factory.set_launch(pipeline)
-        #factory.set_shared(True)
+        factory.set_latency(0)
         mounts.add_factory('/test', factory)
 
-        server.attach()
+        server.attach(None)
         loop = GLib.MainLoop()
 
-        print("Ret loop")
+        print(f"Stream is running on: rtsp://10.0.0.5:{port}/test")
         return loop
+
+
+def run_server(port):
+    server = VideoServerRTSP(port)
+    server.start()
 
 
 if __name__ == '__main__':
     print("Running")
-    server = VideoServerRTSP()
+
+    process1 = multiprocessing.Process(target=run_server, args=('8554',))
+    process2 = multiprocessing.Process(target=run_server, args=('8555',))
+
+    process1.start()
+    process2.start()
+
+    process1.join()
+    process2.join()
